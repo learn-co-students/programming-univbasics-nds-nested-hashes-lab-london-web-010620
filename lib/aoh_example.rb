82eb753c57ef022382d7808b2ca00b8d4e0ffc15
Wojciech Bojarski <bojawojt@gmail.com>
2019-12-21 21:18:56 +0000

    Done.

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -1,17 +1,18 @@
 # Examples inspired by David Foster Wallace's # (2/21/1962 to 9/12/2008) _Infinite Jest_
 		
+	DON_G = { name: "Don Gately", occupation: "Live-in Staff"},
+	JOELLE_VD = { name:  "Joelle van Dyne", occupation: "Radio Personality"},
+	PAT_M = { name: "Pat Monteseian", occupation: "Staff"},
+	KATE_G = { name: "Kate Gompert", occupation: "None" },
+	BRUCE_G = { name: "Bruce Green", occupation: "Fan of Mildred"}
 def assembled_aoh
   # Build an array that contains (or, "nests") the constants into a single
   # Array. Ruby constants are denoted by ALL_CAPS
 
-	don_g = { name: "Don Gately", occupation: "Live-in Staff"},
-	joelle_vd = { name:  "Joelle van Dyne", occupation: "Radio Personality"},
-	pat_m = { name: "Pat Monteseian", occupation: "Staff"},
-	kate_g = { name: "Kate Gompert", occupation: "None" },
-	bruce_g = { name: "Bruce Green", occupation: "Fan of Mildred"}
+	
 
 	# assembled_array = [don_g,joelle_vd,pat_m,kate_g,bruce_g]
-	assembled_array = [don_g,joelle_vd,pat_m,kate_g,bruce_g]
+	assembled_array = [DON_G,JOELLE_VD,PAT_M,KATE_G,BRUCE_G]
 	assembled_array.flatten.to_a
  
 end	
